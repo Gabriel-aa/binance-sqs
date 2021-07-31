64c6fa3618cbc06af69347cb832eab3d5af03243
Gabriel Andrade <gabriel-almeida-andrade@hotmail.com>
2021-07-31 00:17:24 -0300

    chore: testing

--- src/lambda.js
+++ src/lambda.js
@@ -2,10 +2,11 @@ const axios = require('axios');
 
 exports.handler = async (event) => {
   for (const { body } of event.Records) {
+    console.log('processing...');
     const params = JSON.parse(body);
     await axios.post(`${process.env.BINANCE_APP_URL}/trade`, params)
-    .then(function (response) {
-      console.log('foi!', response);
+    .then(function () {
+      console.log('done!');
     })
     .catch(function (error) {
       console.log(error);
